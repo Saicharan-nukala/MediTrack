1f6bb2ad8eef27804ab8ec15e9042f74c4e1856a
Nukala SaiCharan <charan.nukala12@gmail.com>
2025-04-08 11:28:07 +0530

    Updated Profile

--- medi_track/src/App.jsx
+++ medi_track/src/App.jsx
@@ -1,8 +1,6 @@
 import './App.css'
-import Login from'./components/Login'
 import DoctorMain from './components/DoctorMain'
 import Footer from './components/Footer'
-import PatientReportForm from './components/PatientReportForm'
 // import Login from './components/Login'
 
 function App() {
